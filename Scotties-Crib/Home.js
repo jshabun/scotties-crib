2e79da88496273bab589859e02398cf3f0a1e1ff
py1u <peter.lu723@gmail.com>
2024-03-05 18:35:58 -0800

    added nav container and tab for navbar feature(unstable)

---
+++ Scotties-Crib/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, TextInput, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { globalStyles } from './styles';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const HomeScreen = () => {
+  return (
+    
+        <View style={styles.container} >
+        <Text style={{ textAlign: 'center', fontSize: 32, color: 'white'}}>
+            Your Homepage
+        </Text>
+        <NavigationContainer>
+            <TabNavigation/> 
+        </NavigationContainer>
+        </View>
+    
+  );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#0b2138',
+    }
+});
+
+export default HomeScreen;
