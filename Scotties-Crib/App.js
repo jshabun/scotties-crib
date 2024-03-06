8ce698c0936bdf167ba2fcc18273f7b6f3998607
py1u <peter.lu723@gmail.com>
2024-03-06 13:23:12 -0800

    reverted changes for App

--- Scotties-Crib/App.js
+++ Scotties-Crib/App.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import LoginScreen from './Apps/Screens/LoginScreen'
-import { StatusBar } from 'expo-status-bar';
-import { ClerkProvider } from '@clerk/clerk-expo';
-import { SignedIn } from '@clerk/clerk-expo';
-import { SignedOut } from '@clerk/clerk-expo';
 
 const YourApp = () => {
   return (
-    <ClerkProvider publishableKey='pk_test_c3RlcmxpbmctZ3VsbC0xNS5jbGVyay5hY2NvdW50cy5kZXYk'>
-    <View className="flex-1 bg-white">
-      {/* <Text>ScottiesCrib🎉</Text> */}
-      <StatusBar style='auto'/>
-
-        <SignedIn>
-          <Text>You are Signed in</Text>
-        </SignedIn>
-        <SignedOut>
-         <LoginScreen/>
-        </SignedOut>
-      
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Try editing me! 🎉</Text>
     </View>
-    </ClerkProvider>
-
   );
 };
 
