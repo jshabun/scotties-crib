d58eba42d937120fc3fdc610e24cb35cbec24dfa
py1u <peter.lu723@gmail.com>
2024-03-07 11:53:35 -0800

    adjusted NavBar with background styling

--- Scotties-Crib/NavBar.js
+++ Scotties-Crib/NavBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+import { StyleSheet } from 'react-native';
 
 import HomeScreen from './Home';
 import ProfileScreen from './Profile';
@@ -10,14 +12,53 @@ const Tab = createBottomTabNavigator();
 
 const NavBar = () => {
   return (
-    
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-        <Tab.Screen name="Upload" component={UploadScreen} />
-      </Tab.Navigator>
-    
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Upload') {
+            iconName = 'add-circle';
+          } else if (route.name === 'Profile') {
+            iconName = 'person-circle';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarLabelStyle: styles.label,
+        tabBarItemStyle: styles.tab,
+        tabBarStyle: [
+          styles.tabBar,
+          null,
+        ],
+        headerShown: false,
+      })}
+      tabBarOptions={{
+        activeTintColor: '#FFE082', // Change the active tab color
+        inactiveTintColor: 'white', // Change the inactive tab color
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Upload" component={UploadScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  tab: {
+    paddingTop: 10,
+  },
+  label: {
+    fontSize: 12,
+  },
+  tabBar: {
+    height:80,
+    display: 'flex',
+    backgroundColor: '#0b2138',
+  },
+});
 
 export default NavBar;
